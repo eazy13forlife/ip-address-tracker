ceb31f35c44ba9b96e3ed0e657f9b49253bb1979
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-07-06 20:40:15 -0700

    changed from https to http

--- src/actions/ipData.js
+++ src/actions/ipData.js
@@ -6,7 +6,7 @@ import { updateLatLon } from "./latlon.js";
 const fetchIpData = (ipAddress) => {
   return async (dispatch) => {
     try {
-      const response = await axios.get(`https://ip-api.com/json/${ipAddress}`);
+      const response = await axios.get(`http://ip-api.com/json/${ipAddress}`);
 
       //if successful, call updateLatLon so we can use those lat/lon values for our leaflet map. If not successful,dont update anything. Our leaflet map will use the most recent successful lat/lon values
       const { status, lat, lon } = response.data;
